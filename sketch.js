3ef72f6f37e3b7ca399adf04b1e3da39d8a37c39
vinnu <venkatavineela3@gmail.com>
2021-02-02 17:54:20 +0530

    Add Platform

--- sketch.js
+++ sketch.js
@@ -16,7 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform =new Ground(150,100,30,30);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
